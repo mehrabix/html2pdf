e62780d7f26d1e6bd98878ae012df706437b3f3a
a.mehrabi <a.mehrabi@demisco.com>
2025-07-08 13:06:50 +0330

    Initialize Spring Boot project with basic structure, including .gitignore, pom.xml, main application class, and application properties file.

---
+++ src/main/java/com/easymed/html2pdf/Html2pdf.java
@@ -0,0 +1,16 @@
+package com.easymed.html2pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Html2pdf {
+    public static void main(String[] args) {
+        SpringApplication.run(Html2pdf.class, args);
+    }
+}
+
+
+
+
+
